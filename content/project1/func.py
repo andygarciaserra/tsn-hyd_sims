048f946bf759cc6b90f2eda561ede540ad718315
andygarciaserra <alu0101451923@ull.edu.es>
2022-04-05 13:59:56 +0100

    NFW fitting

--- content/project1/func.py
+++ content/project1/func.py
@@ -3,5 +3,8 @@ import numpy as np
 def logNFW(r,rho0,Rs):
     return np.log10(rho0)-(np.log10(r/Rs)+2*np.log10(1+r/Rs))
 
+def NFW(r,rho0,Rs):
+    return rho0/((r/Rs)*(1+r/Rs)**2)
+
 def distance(c0,x0,c1,x1,c2,x2):
     return np.sqrt((abs(c0-x0)**2)+(abs(c1-x1)**2)+(abs(c2-x2)**2))
